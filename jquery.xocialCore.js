b2afc356ec9a931ed4443b8d5a1452d24f77c606
XocialHost <xocialhost@personal-fx.com>
2011-07-23 12:06:05 -0600

    More updates

--- jquery.xocialCore.js
+++ jquery.xocialCore.js
@@ -2,7 +2,27 @@
 *  Plugin to access OpenMenu
 *  version 0.0.1, July 12, 2011
 *  by Dustin Nielson - http://www.xocialhost.com
-*  copyright: Copyright (c) 2011 Dustin Nielson for Xocial Host
+
+	Copyright (C) 2011 Dustin Nielson for Xocial Host except as noted by attributions.
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
 *  This plugin incorporates some revised MIT licensed plugins so some 
 *  sections Copyright the orignal authors.
 *  license: MIT
@@ -20,7 +40,7 @@ $.xcInitFacebook = function(options){
 	
 	var settings = {
 	  
-	  'apiKey'		:	null,
+	  'appId'		:	null,
 	  'callback'	:	null,
 	  'channelUrl'  :   null,
 	  'status'		:	true,
@@ -37,11 +57,11 @@ $.xcInitFacebook = function(options){
 	
 		if(settings.channelUrl==null) {
 	
-			FB.init({appId: settings.apiKey, status: settings.status, cookie: settings.cookie, xfbml: settings.xfbml, oauth: true, authResponse: true }); 
+			FB.init({appId: settings.appId, status: settings.status, cookie: settings.cookie, xfbml: settings.xfbml, oauth: true, authResponse: true }); 
 			
 		} else {
 			
-			FB.init({appId: settings.apiKey, status: settings.status, cookie: settings.cookie, xfbml: settings.xfbml, oauth: true, authResponse: true, channelUrl: settings.channelUrl }); 
+			FB.init({appId: settings.appId, status: settings.status, cookie: settings.cookie, xfbml: settings.xfbml, oauth: true, authResponse: true, channelUrl: settings.channelUrl }); 
 		}
 		
 		if(typeof settings.callback == 'function'){ settings.callback.call(this); }
@@ -537,7 +557,7 @@ $.fn.xcAjax = function (options) {
 		  
 	$.ajax({
 		
-	  dataType: dataType,
+	  dataType: settings.dataType,
 	  type: 'POST',
 	  data: settings.params,
 	  url: settings.url,
