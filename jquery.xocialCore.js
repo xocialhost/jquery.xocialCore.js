7b12e85fe0671a37e026d1502bd2b379e81fad3b
XocialHost <xocialhost@personal-fx.com>
2011-07-30 17:50:19 -0600

    fixes

--- jquery.xocialCore.js
+++ jquery.xocialCore.js
@@ -610,7 +610,7 @@ $.xcGetPhotos = function(options) {
 		
 		if(typeof (settings.callback) == 'function') {
 					
-			settings.callback.call(this, data.data);
+			settings.callback.call(this, data);
 		
 		} else { return false; }
 		
